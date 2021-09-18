1ba65740420918b0774f4a84043c69ec98b89c7d
Chetan Dhangar <chetansdhangar@gmail.com>
2021-09-18 13:22:01 +0530

    feat - profile timeline added

--- src/features/user/components/Profile/ProfileTimeLine.js
+++ src/features/user/components/Profile/ProfileTimeLine.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import {UserProfileTweet} from '../../components/BookMark/UserProfileTweet' 
 
 export const ProfileTimeLine = ({user}) => {
     return(
         <div>
-            Profile TimeLine
+             {user?.tweets?.length <= 0 ? (
+                    <div>Don't hava any BookMarks yet</div>
+                ): 
+                (
+                    user?.tweets?.map((tweet) => {
+                        return(
+                            <UserProfileTweet key={tweet?._id} tweet={tweet}/>
+                        )
+                    })
+                )
+                }
         </div>
     )
 }
