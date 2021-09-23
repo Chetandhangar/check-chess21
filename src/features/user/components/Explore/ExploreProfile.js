9955f1bb33b0f818f19eeab4354decf4139aae3c
Chetan Dhangar <chetansdhangar@gmail.com>
2021-09-23 11:50:35 +0530

    style - explore page

--- src/features/user/components/Explore/ExploreProfile.js
+++ src/features/user/components/Explore/ExploreProfile.js
@@ -1,35 +1,43 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {useSelector} from 'react-redux';
-import {FollowButton} from '../Buttons/FollowButton'
+import {FollowButton} from '../Buttons/FollowButton';
+import useStyles from './ExploreStyles';
+import {Card,CardHeader,Avatar,IconButton} from '@material-ui/core'
 
 export const ExploreProfile = ({userDetails , user}) => {
     const {currentUser} = useSelector((state) => state.user)
+    const classes = useStyles()
     return(
-     <div className="container"> 
-     <div className="card">
-        <Link to={`/profile/${user?.username}`}>
-            <img 
-            style={{height: 20, width : 20}}
-                className="rounded-circle rounded-sm inline-block" 
-                src='https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png'
-                alt="profile"
-                />
-        </Link>
-        <Link 
-        to={`/profile/${user?.username}`}
-        >
-        <h5 className="card-title">{user?.firstName}{" "} {user?.lastName}</h5>
-        <span className="card-subtitle">@{user?.username}</span>
-        </Link>
-        <div>
-           {currentUser?._id !== user._id && (
+        <div className={classes.paper}>
+        <Card className={classes.root}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="recipe" className={classes.avatar}>
+              <Link to={`/profile/${user?.username}`}>
+              <img  
+              style={{height: 35, width : 35}}
+              src='https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png' 
+              alt="Profile"/>
+             </Link>
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="settings">
+               {currentUser?._id !== user._id && (
                <FollowButton user={user}/>
-           )
-           }
+             )
+             }
+            </IconButton>
+          }
+          title={user?.firstName && <Link to={`/profile/${user?.username}`}>
+             {user?.firstName} {" "} {user?.lastName}
+          </Link>}
+          subheader={user?.username && <Link to={`/profile/${user?.username}`}>
+              {`@${user?.username}`}
+          </Link>}
+        />
+        </Card>
         </div>
-     </div>
-    </div>
- 
     )
 }
